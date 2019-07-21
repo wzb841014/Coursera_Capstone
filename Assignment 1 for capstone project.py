983bfefcea265e1689b55d61a923f70824d1f3b5
wzb841014 <50065751+wzb841014@users.noreply.github.com>
2019-07-22 09:41:59 +1000

    Add file vs upoad

---
+++ Assignment 1 for capstone project.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#This notebook is mainly used for capstone project.
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
